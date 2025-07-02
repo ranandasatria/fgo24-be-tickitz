e970b9797b4b071aef31cf22301410fdc1c406b9
ranandasatria <rananda24@gmail.com>
2025-07-02 14:00:38 +0700

    feat: create parsejwt function

--- utils/generatetoken.go
+++ utils/generatetoken.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"os"
 	"time"
 
@@ -20,4 +21,26 @@ func GenerateJWT(purpose string, userId int, expiry time.Duration) (string, erro
 	})
 
 	return token.SignedString([]byte(secret))
+}
+
+func ParseJWT(tokenStr string) (jwt.MapClaims, error) {
+	godotenv.Load()
+	secret := os.Getenv("APP_SECRET")
+
+	token, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return claims, nil
+	}
+
+	return nil, fmt.Errorf("invalid token")
 }
