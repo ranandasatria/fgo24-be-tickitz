87d41c642550933ecca66f1147f10fdaec0229ce
ranandasatria <rananda24@gmail.com>
2025-07-12 19:09:31 +0700

    refactor: revamp connectdb pool

--- utils/db.go
+++ utils/db.go
@@ -4,26 +4,48 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sync"
+	"time"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 )
 
+var (
+	pool *pgxpool.Pool
+	once sync.Once
+)
+
 func ConnectDB() (*pgxpool.Conn, error) {
-	godotenv.Load()
+	once.Do(func() {
+		godotenv.Load()
+		connStr := fmt.Sprintf(
+			"postgres://%s:%s@%s:%s/%s",
+			os.Getenv("PGUSER"),
+			os.Getenv("PGPASSWORD"),
+			os.Getenv("PGHOST"),
+			os.Getenv("PGPORT"),
+			os.Getenv("PGDATABASE"),
+		)
 
-	connStr := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s",
-		os.Getenv("PGUSER"),
-		os.Getenv("PGPASSWORD"),
-		os.Getenv("PGHOST"),
-		os.Getenv("PGPORT"),
-		os.Getenv("PGDATABASE"),
-	)
+		config, err := pgxpool.ParseConfig(connStr)
+		if err != nil {
+			fmt.Println("Error parsing config:", err)
+			return
+		}
 
-	pool, err := pgxpool.New(context.Background(), connStr)
-	if err != nil {
-		return nil, err
+		config.MaxConns = 20
+		config.MinConns = 2
+		config.MaxConnLifetime = 30 * time.Minute
+
+		pool, err = pgxpool.NewWithConfig(context.Background(), config)
+		if err != nil {
+			fmt.Println("Error creating pool:", err)
+		}
+	})
+
+	if pool == nil {
+		return nil, fmt.Errorf("database pool not initialized")
 	}
 
 	conn, err := pool.Acquire(context.Background())
